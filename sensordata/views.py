dd559e5e206d60a5dbea3db108c82d234aa6eeee
Vibhor Khurana <vibhorkhurana@gmail.com>
2017-06-07 15:19:57 +0530

    Sample Django Rest Framework2..v4 with more generics and reduced code

--- sensordata/views.py
+++ sensordata/views.py
@@ -1,14 +1,6 @@
 from django.shortcuts import render
-#from django.shortcuts import render_to_response
 import json
-from django.http import HttpResponse, JsonResponse, Http404
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
-#from rest_framework.parsers import JSONParser
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from sensordata.models import Sensor
 from sensordata.serializers import sensorSerializer
 from rest_framework import mixins
@@ -23,36 +15,13 @@ def index(request):
         'sensors': sensie,
     })
 
-class SensorList(mixins.ListModelMixin,
-                 mixins.CreateModelMixin,
-                 generics.GenericAPIView):
+class SensorList(generics.ListCreateAPIView):
     queryset=Sensor.objects.all()
     serializer_class=sensorSerializer
 
-    def get(self,request,*args,**kwargs):
-        return self.list(request,*args,**kwargs)
-
-    def post(self,request,*args,**kwargs):
-        return self.create(request,*args,**kwargs)
-
-
-class SensorDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class SensorDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a sensor instance
     """
-
     queryset=Sensor.objects.all()
     serializer_class=sensorSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
