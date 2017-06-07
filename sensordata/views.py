b45008eadfcff9e6be61c05ca73015eca854c13a
Vibhor Khurana <vibhorkhurana@gmail.com>
2017-06-07 14:43:59 +0530

    Sample Django Rest Framework2..v3 with more generics

--- sensordata/views.py
+++ sensordata/views.py
@@ -11,7 +11,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from sensordata.models import Sensor
 from sensordata.serializers import sensorSerializer
-
+from rest_framework import mixins
+from rest_framework import generics
 
 def index(request):
     sensors = Sensor.objects.all()
@@ -22,61 +23,36 @@ def index(request):
         'sensors': sensie,
     })
 
-class SensorList(APIView):
-    #@csrf_exempt
-    # @api_view(['GET','POST'])
-    # def sensor_data(request,format=None):
-    #     """
-    #     List all sensor data.
-    #     """
-    #     if request.method == 'GET':
-    #         sensors = Sensor.objects.all()
-    #         serializer = sensorSerializer(sensors,many=True)
-    #         return Response(serializer.data)
-    #     elif request.method == 'POST':
-    #         serializer = sensorSerializer(data=request.data)
-    #         if serializer.is_valid():
-    #             serializer.save()
-    #             return Response(serializer.data,status=status.HTTP_201_CREATED)
-    #         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+class SensorList(mixins.ListModelMixin,
+                 mixins.CreateModelMixin,
+                 generics.GenericAPIView):
+    queryset=Sensor.objects.all()
+    serializer_class=sensorSerializer
 
-    def get(self,request,format=None):
-        sensors=Sensor.objects.all()
-        serializer = sensorSerializer(sensors, many=True)
-        return Response(serializer.data)
+    def get(self,request,*args,**kwargs):
+        return self.list(request,*args,**kwargs)
 
-    def post(self,request,format=None):
-        serializer = sensorSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    def post(self,request,*args,**kwargs):
+        return self.create(request,*args,**kwargs)
 
-class SensorDetail(APIView):
+
+class SensorDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
     """
     Retrieve, update or delete a sensor instance
     """
-    def get_sensor(self,pk):
-        try:
-            return Sensor.objects.get(pk=pk)
-        except Sensor.DoesNotExist:
-            raise Http404
 
-    def get(self, request, pk, format=None):
-        sensor = self.get_sensor(pk)
-        serializer = sensorSerializer(sensor, many=False)
-        return Response(serializer.data)
+    queryset=Sensor.objects.all()
+    serializer_class=sensorSerializer
 
-    def put(self,request, pk, format=None):
-        sensor = self.get_sensor(pk)
-        serializer = sensorSerializer(sensor, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def delete(self, request, pk, format=None):
-        sensor = self.get_sensor(pk)
-        sensor.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
 
